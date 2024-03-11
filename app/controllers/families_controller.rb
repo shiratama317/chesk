8189c4bd52e61c0feba5367da049fcbd69b12635
shiratama317 <shiratamax6x5@gmail.com>
2024-03-12 02:02:01 +0900

    ファミリー新規作成画面の実装

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,4 +1,13 @@
 class FamiliesController < ApplicationController
   def top
   end
+
+  def new
+    @family = current_user.families.build
+  end
+
+  private
+  def family_params
+    params.require(:family).permit(:name)
+  end
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,6 @@
 class Family < ApplicationRecord
   has_many :family_users, dependent: :destroy
   has_many :users, through: :family_users
+
+  validates :name, presence: true, length: { maximum: 15 }
 end
