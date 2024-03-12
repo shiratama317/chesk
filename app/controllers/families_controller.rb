300c678e8e4246a6f78942d4045fd5f3b4ab27c7
shiratama317 <shiratamax6x5@gmail.com>
2024-03-12 14:34:14 +0900

    ファミリーの新規作成・保存

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -6,6 +6,14 @@ class FamiliesController < ApplicationController
     @family = current_user.families.build
   end
 
+  def create
+    @family = Family.new(family_params)
+    @family.users << current_user
+    if @family.save
+      redirect_to root_path
+    end
+  end
+
   private
   def family_params
     params.require(:family).permit(:name)
