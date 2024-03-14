fee4fbed78b73c9dd265d3d04207daafb0622cdf
shiratama317 <shiratamax6x5@gmail.com>
2024-03-15 02:48:53 +0900

    taskモデル・テーブルの作成

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,6 +1,7 @@
 class Family < ApplicationRecord
   has_many :family_users, dependent: :destroy
   has_many :users, through: :family_users
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 15 }
 end
