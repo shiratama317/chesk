eac881ae9c9036b181ffeda384064dfdde5b98a7
shiratama317 <shiratamax6x5@gmail.com>
2024-03-15 03:38:44 +0900

    ActiveHashの導入・設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :family
 
   validates :contents, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
