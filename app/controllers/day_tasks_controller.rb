bb16e200e59210d30a3bcc4e2a64e3b61b33028f
shiratama317 <shiratamax6x5@gmail.com>
2024-03-15 23:10:59 +0900

    DayTaskの新規作成・保存

--- app/controllers/day_tasks_controller.rb
+++ app/controllers/day_tasks_controller.rb
@@ -1,12 +1,27 @@
 class DayTasksController < ApplicationController
+  before_action :day_task_new, only: [:new, :create]
+  
   def new
-    @family = Family.find(params[:family_id])
     @day_task = DayTask.new
+  end
+
+  def create
+    @day_task = DayTask.new(day_task_params)
+    if @day_task.save
+      redirect_to new_family_day_task_path(params[:family_id]), success: '設定しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def day_task_new
+    @family = Family.find(params[:family_id])
     @tasks = @family.tasks
     @users = @family.users
   end
 
-  def create
-    @day_task = DayTask.new
+  def day_task_params
+    params.require(:day_task).permit(:action_day, :task_id, :user_id)
   end
 end
--- app/models/day_task.rb
+++ app/models/day_task.rb
@@ -1,4 +1,10 @@
 class DayTask < ApplicationRecord
   belongs_to :task
   belongs_to :user
+
+  with_options presence: true do
+    validates :action_day
+    validates :task_id
+    validates :user_id
+  end
 end
