01a181453738ac0a309490c5abf47d1e49fb9465
shiratama317 <shiratamax6x5@gmail.com>
2024-03-20 03:07:28 +0900

    フラッシュメッセージの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(current_user.id)
     if @user.destroy
-      redirect_to root_path, success: "退会しました"
+      redirect_to root_path
     else
       redirect_to root_path, danger: "退会できませんでした"
     end
