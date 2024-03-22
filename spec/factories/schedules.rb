011f73c4783be6c597bb38418f58360e261f8911
shiratama317 <shiratamax6x5@gmail.com>
2024-03-22 23:29:30 +0900

    Scheduleモデル単体テスト

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :schedule do
-    
+    event { Faker::Lorem.sentence(word_count: 3) }
+    start_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 5) }
+    end_time   { Faker::Time.between(from: start_time, to: start_time + 5) }
+    association :family
+    association :user
   end
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @schedule = FactoryBot.build(:schedule)
+  end
+
+  describe 'Schedule新規登録機能' do
+    context '新規登録できるとき' do
+      it '必要な情報がすべて存在している場合は登録できる' do
+        expect(@schedule).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'eventが空では登録できない' do
+        @schedule.event = ''
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include("Event can't be blank")
+      end
+      it 'start_timeが空では登録できない' do
+        @schedule.start_time = ''
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include("Start time can't be blank")
+      end
+      it 'end_timeが空では登録できない' do
+        @schedule.end_time = ''
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include("End time can't be blank")
+      end
+      it 'end_timeがstart_time以前では登録できない' do
+        @schedule.end_time = Faker::Time.between(from: @schedule.start_time - 5, to: @schedule.start_time)
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include("End time は開始時刻以降で設定してください")
+      end
+      it 'user_idが紐づいていなければ、登録できない' do
+        @schedule.user = nil
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include('User must exist')
+      end
+      it 'family_idが紐づいていなければ、登録できない' do
+        @schedule.family = nil
+        @schedule.valid?
+        expect(@schedule.errors.full_messages).to include('Family must exist')
+      end
+    end
+  end
 end
